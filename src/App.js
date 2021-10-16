29e8de5c322836abaf69dd1fa11bfef8ea15e4b3
Abhishek <abhishekkuttikkol007@gmail.com>
2021-10-16 16:28:37 +0530

    Animation bug solved

--- src/App.js
+++ src/App.js
@@ -9,66 +9,117 @@ import Works from "./Components/Works";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
-
 function App() {
   useEffect(() => {
-    const showMenu = (toggleId, navId) => {
-      const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId);
+    // const showMenu = (toggleId, navId) => {
+    //   const toggle = document.getElementById(toggleId),
+    //     nav = document.getElementById(navId);
 
-      if (toggle && nav) {
-        toggle.addEventListener("click", () => {
-          nav.classList.toggle("show");
-        });
-      }
-    };
-    showMenu("nav-toggle", "nav-menu");
+    //   if (toggle && nav) {
+    //     toggle.addEventListener("click", () => {
+    //       nav.classList.toggle("show");
+    //     });
+    //   }
+    // };
+    // showMenu("nav-toggle", "nav-menu");
 
-    /*===== ACTIVE AND REMOVE MENU =====*/
-    const navLink = document.querySelectorAll(".nav__link");
+    // /*===== ACTIVE AND REMOVE MENU =====*/
+    // const navLink = document.querySelectorAll(".nav__link");
 
-    function linkAction() {
-      /*Active link*/
-      navLink.forEach((n) => n.classList.remove("active"));
-      this.classList.add("active");
+    // function linkAction() {
+    //   /*Active link*/
+    //   navLink.forEach((n) => n.classList.remove("active"));
+    //   this.classList.add("active");
 
-      /*Remove menu mobile*/
-      const navMenu = document.getElementById("nav-menu");
-      navMenu.classList.remove("show");
-    }
-    navLink.forEach((n) => n.addEventListener("click", linkAction));
+    //   /*Remove menu mobile*/
+    //   const navMenu = document.getElementById("nav-menu");
+    //   navMenu.classList.remove("show");
+    // }
+    // navLink.forEach((n) => n.addEventListener("click", linkAction));
 
     /*===== SCROLL REVEAL ANIMATION =====*/
     const sr = ScrollReveal({
       origin: "top",
       distance: "80px",
-      duration: 650,
+      // duration: 600,
       reset: true,
-      easing: 'ease-in'
+      easing: "ease-in",
     });
 
     /*SCROLL HOME*/
-    sr.reveal(".home__title", { delay: 100 });
-    sr.reveal(".button", { delay: 100 });
-    sr.reveal(".home__img", { delay: 200 });
-    sr.reveal(".home__social-icon", { interval: 100 });
+    sr.reveal(".home__title", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
+    sr.reveal(".button", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
+    sr.reveal(".home__img", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
+    sr.reveal(".home__social-icon", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
 
     /*SCROLL ABOUT*/
-    sr.reveal(".about__img", { delay: 100 });
-    sr.reveal(".about__subtitle", { delay: 200 });
-    sr.reveal(".about__text", { delay: 200 });
+    sr.reveal(".about__img", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
+    sr.reveal(".about__subtitle", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
+    sr.reveal(".about__text", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
 
     /*SCROLL SKILLS*/
-    sr.reveal(".skills__subtitle", { delay: 100 });
-    sr.reveal(".skills__text", { delay: 100 });
-    sr.reveal(".skills__data", { interval: 100 });
-    sr.reveal(".skills__img", { delay: 300 });
+    sr.reveal(".skills__subtitle", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
+    sr.reveal(".skills__text", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
+    sr.reveal(".skills__data", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
+    sr.reveal(".skills__img", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
 
     /*SCROLL WORK*/
-    sr.reveal(".work__img", { interval: 100 });
+    sr.reveal(".work__img", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
 
     /*SCROLL CONTACT*/
-    sr.reveal(".contact__input", { interval: 200 });
+    sr.reveal(".contact__input", {
+      delay: 375,
+      duration: 500,
+      reset: true,
+    });
   }, []);
 
   return (
